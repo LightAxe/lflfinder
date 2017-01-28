5cf6d4d7cd3bf5050e191bdfa4fa6e9b2e2bd280
LightAxe <rob@axpr.net>
2017-01-27 21:58:40 -0800

    Further defined library type; add and view libraries

--- server/lll/app/controllers/libraries_controller.rb
+++ server/lll/app/controllers/libraries_controller.rb
@@ -1,8 +1,33 @@
 class LibrariesController < ApplicationController
+  def index
+    @libraries = Library.all
+  end
+
+  def show
+    @library = Library.find(params[:id])
+  end
+
   def new
   end
 
-  def create
-    render plain: params[:librarie].inspect
+  def edit
   end
+
+  def create
+    @library = Library.new(library_params)
+
+    @library.save
+    redirect_to @library
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def library_params
+      params.require(:library).permit(:name, :description)
+    end
 end
